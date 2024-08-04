18d4e22e023be677ba77269b9e4fde824c68ced5
shelep-olya <142796512+shelep-olya@users.noreply.github.com>
2024-08-04 19:55:35 +0300

    powerOfTwo

---
+++ powerOfTwo.java
@@ -0,0 +1,10 @@
+// Power of Two
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        return (n & (n - 1)) == 0;
+    }
+}
