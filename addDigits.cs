6d34adda74f129f76fc2ea786d38e1ddb3ef84e9
shelep-olya <142796512+shelep-olya@users.noreply.github.com>
2024-08-05 15:18:51 +0300

    addDigits

---
+++ addDigits.cs
@@ -0,0 +1,9 @@
+//  Add Digits
+
+public class Solution {
+    public int AddDigits(int num) {
+        if (num == 0) return 0;
+        if (num % 9 == 0) return 9;
+        return num % 9;
+    }
+}
