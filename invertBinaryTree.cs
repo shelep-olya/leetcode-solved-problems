dd2593b127ebcad18d391166ec707a6d34963f76
shelep-olya <142796512+shelep-olya@users.noreply.github.com>
2024-08-08 16:12:39 +0300

    invertBinaryTree

---
+++ invertBinaryTree.cs
@@ -0,0 +1,15 @@
+//  Invert Binary Tree
+
+public class Solution {
+    public TreeNode InvertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+          InvertTree(root.left);
+        InvertTree(root.right);
+        return root;
+    }
+}
