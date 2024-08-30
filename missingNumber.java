5aeea6b0a5c92b3357d2a4fba85ca8c94e807ef4
shelep-olya <142796512+shelep-olya@users.noreply.github.com>
2024-08-30 13:23:55 +0300

    missingNumber

---
+++ missingNumber.java
@@ -0,0 +1,18 @@
+// Missing Number
+
+import java.util.HashSet;
+import java.util.Set;
+class Solution {
+    public int missingNumber(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int num : nums){
+            set.add(num);
+        }
+        for(int i = 0; i < nums.length + 1; i ++){
+            if(!set.contains(i)){
+                return i;
+            }
+        }
+       return -1;
+    }
+}
